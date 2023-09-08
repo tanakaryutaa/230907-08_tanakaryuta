e15e09b1082b54a80fa84a26c0d767667b13aca3
tanakaryutaa <ryutakun121202@gmail.com>
2023-09-08 13:13:22 +0900

    add; SAKURUG2daysIS

---
+++ src/main/java/com/todo/app/entity/Todo.java
@@ -0,0 +1,13 @@
+package com.todo.app.entity;
+
+import lombok.Data;
+
+@Data
+public class Todo {
+
+    private long id;
+    private String title;
+    private int done_flg;
+    private String time_limit;
+    private String category;
+}
